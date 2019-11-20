d3fbb3fd8c52f334c89011d2a703afffebc47974
wh0dat <whodat@remainsilent.net>
2019-11-21 03:30:35 +0530

    sdm632-common: bluetooth: disable clean turn on * skips conn update at conn completion
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -50,6 +50,8 @@ static inline const char* BtmGetDefaultName()
 #define BTM_WBS_INCLUDED TRUE
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
+// skips conn update at conn completion
+#define BT_CLEAN_TURN_ON_DISABLED 1
 
 /* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
 #define AVDT_NUM_SEPS                  12
